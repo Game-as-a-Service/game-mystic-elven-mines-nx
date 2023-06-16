332813ca4be7519bcc689a5fd8391d1a98b4b171
day.ch <day.ch.tcg@gmail.com>
2023-06-16 19:16:21 +0800

    [Frontend] Fix: update api url

--- packages/frontend/src/core/network/api/index.tsx
+++ packages/frontend/src/core/network/api/index.tsx
@@ -8,13 +8,13 @@ let gameId = 51651561561;
 
 // [GET] 建立遊戲
 export const createGame = async (gameId: string = 'gameId01') => {
-  const url = `${API_URL}/api/test/createGame/${gameId}`;
+  const url = `${API_URL}test/createGame/${gameId}`;
   return await fetcher(url);
 };
 
 // [GET] 查詢遊戲
 export const findGame = async (gameId: string = 'gameId01') => {
-  const url = `${API_URL}/api/test/findGame/${gameId}`;
+  const url = `${API_URL}test/findGame/${gameId}`;
   return await fetcher(url);
 };
 
@@ -26,30 +26,30 @@ export const getHandCards = async () => {
 
 // [POST] 玩家打牌
 export const playCard = async (body: IplayCard) => {
-  const url = `${API_URL}/api/games/${gameId}:playCard`;
+  const url = `${API_URL}games/${gameId}:playCard`;
   return await fetcher(url, body);
 };
 
 // [POST] 玩家棄牌
 export const foldCard = async (body: IplayCard) => {
-  const url = `${API_URL}/api/games/${gameId}:foldCard`;
+  const url = `${API_URL}games/${gameId}:foldCard`;
   return await fetcher(url, body);
 };
 
 // [GET] 取得地圖資訊
 export const mapCards = async (body: IplayCard) => {
-  const url = `${API_URL}/api/games/${gameId}/mapCards`;
+  const url = `${API_URL}games/${gameId}/mapCards`;
   return await fetcher(url, body);
 };
 
 // [GET] 取得玩家資訊
 export const players = async (body: IplayCard) => {
-  const url = `${API_URL}/api/games/${gameId}/players`;
+  const url = `${API_URL}games/${gameId}/players`;
   return await fetcher(url, body);
 };
 
 // TEST 測試後端有沒有正常運作
 export const helloTest = async () => {
-  const url = `${API_URL}/api/test/hello`;
+  const url = `${API_URL}test/hello`;
   return await fetcher(url);
 };
