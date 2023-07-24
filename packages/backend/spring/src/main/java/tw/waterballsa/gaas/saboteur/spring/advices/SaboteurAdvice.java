089bf1d81039bd57ee490145d59534b87f01f779
Jacky0428 <jt26419701@yahoo.com.tw>
2023-07-25 00:05:06 +0800

    RestControllerAdvice add SaboteurGameException

--- packages/backend/spring/src/main/java/tw/waterballsa/gaas/saboteur/spring/advices/SaboteurAdvice.java
+++ packages/backend/spring/src/main/java/tw/waterballsa/gaas/saboteur/spring/advices/SaboteurAdvice.java
@@ -8,13 +8,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tw.waterballsa.gaas.saboteur.domain.exceptions.NotFoundException;
+import tw.waterballsa.gaas.saboteur.domain.exceptions.SaboteurGameException;
 
 import static java.util.stream.Collectors.joining;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class SaboteurAdvice {
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler({SaboteurGameException.class})
+    public String badRequest(SaboteurGameException exception) {
+        return exception.getMessage();
+    }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public String handleValidationExceptions(MethodArgumentNotValidException ex) {
         return ex.getBindingResult()
@@ -25,13 +32,13 @@ public class SaboteurAdvice {
             .collect(joining("\n"));
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler({NotFoundException.class})
     public String notFound(NotFoundException exception) {
         return exception.getMessage();
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public String otherException(Exception exception) {
         return exception.getMessage();
